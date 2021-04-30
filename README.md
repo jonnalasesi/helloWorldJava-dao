b9ef3771579c2969641fc8e1b9eac23dbc1498d7
jonnalasesi <sesi.jonnala487@topper.wku.edu>
2021-04-29 21:37:10 -0500

    initial commit

--- README.md
+++
@@ -1,9 +0,0 @@
-# mysqlmicroservice
-
-```
-Initial mysql setup :
-Download MySQL Community Server : https://dev.mysql.com/downloads/mysql/
-Mysql workbench: https://dev.mysql.com/downloads/workbench/
-DB Viewer : https://dbeaver.io/download/
-
-```
---
+++ src/main/java/com/helloworld/java/dao/HelloWorldJavaApplication.java
@@ -0,0 +1,13 @@
+package com.helloworld.java.dao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldJavaApplication.class, args);
+    }
+
+}
